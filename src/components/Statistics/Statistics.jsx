1b891eee361acfdcb31f89b7b2859d8cc5e03fa9
dozeran <130239890+dozeran@users.noreply.github.com>
2023-10-13 22:09:41 +0300

    summary

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+export const Statistics = ({ data }) => { 
+
+    data.map(({id, label, percentage}) => {
+        return (
+            <li className="item" key={id}>
+      <span className="label">{label}</span>
+                <span className="percentage">{percentage}</span>
+    </li>
+        )
+    })
+}
